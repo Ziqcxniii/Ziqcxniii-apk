43bcd66ceecb89b46426d2240cba16363f04ec1b
Ziqcxniii <alifnibos321@gmail.com>
2026-09-03 20:56:52 +0800

    Create MainActivity.java

---
+++ app/src/main/java/com/ziqcxniii/ziqcxniii/MainActivity.java
@@ -0,0 +1,36 @@
+package com.ziqcxniii.ziqcxniii;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://aliptlhackv2.netlify.app/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
